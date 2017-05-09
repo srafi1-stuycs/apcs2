741382075ff72a6718e37f2815f4f8f2f3c0de17
srafi1 <shakilrafi0@gmail.com>
2017-05-09 16:19:18 -0400

    Automatic commit message

--- hw37/ALHeap.java
+++ hw37/ALHeap.java
@@ -28,11 +28,8 @@ public class ALHeap
      *****************************************************/
     public String toString() 
     { 
-	String ret = "";
-	for (int i : _heap)
-	    ret += i + " ";
-	return ret;
-    }//O(1)
+	return _heap.toString();
+    }//O(n)
 
 
 
@@ -58,7 +55,6 @@ public class ALHeap
     } //O(1)
 
 
-
     /*****************************************************
      * add(Integer) 
      * Inserts an element in the heap
@@ -66,8 +62,22 @@ public class ALHeap
      *****************************************************/
     public void add( Integer addVal ) 
     { 
-	
-    } //O(?)
+	_heap.add(addVal); //starts off at end
+	int level = 1;
+	while (Math.pow(2, level) - 1 < _heap.size()) {
+	    level++; //find level of new element
+	}
+	int myIndex = _heap.size() - 1; //current index of new element
+	int subIndex = myIndex - (int) Math.pow(2, level - 1) - 1; //index of element within level
+	int parentIndex = (int) Math.pow(2, level - 2) + (subIndex - 1)/2;
+	while (myIndex > 0 && parentIndex >= 0 && _heap.get(myIndex) < _heap.get(parentIndex)) {
+	    swap(myIndex, parentIndex);
+	    myIndex = parentIndex; //update new element's index
+	    level--;
+	    subIndex = myIndex - (int) Math.pow(2, level - 1) - 1;
+	    parentIndex = (int) Math.pow(2, level - 2) + (subIndex - 1)/2;
+	}
+    } //O(n)
 
 
 
@@ -78,8 +88,22 @@ public class ALHeap
      *****************************************************/
     public Integer removeMin() 
     {
-
-    }//O(?)
+	if (_heap.size() == 1)
+	    return _heap.remove(0);
+	else if (_heap.size() == 0)
+	    return null;
+	int ret = _heap.get(0);
+	_heap.set(0, _heap.remove(_heap.size() - 1));
+	int index = 0;
+	while (minChildPos(index) != -1) {
+	    int newIndex = minChildPos(index);
+	    if (_heap.get(index) < _heap.get(newIndex))
+		break;
+	    swap(index, newIndex);
+	    index = newIndex;
+	}
+	return ret;
+    }//O(n)
 
 
 
@@ -89,10 +113,26 @@ public class ALHeap
      * -1 if no children, or if input pos is not in ArrayList
      * Postcondition: Tree unchanged
      *****************************************************/
-    private int minChildPos( int pos ) 
+    public int minChildPos( int pos ) 
     {
-
-    }//O(?)
+	if (pos < 0 || pos >= _heap.size())
+	    return -1;
+	int level = 1;
+	while (Math.pow(2, level) - 1 < pos) {
+	    level++; //find level of current element
+	}
+	int subIndex = pos - (int) (Math.pow(2, level - 1) - 1);
+	int childIndex1 = (int) (Math.pow(2, level) - 1) + subIndex*2;
+	int childIndex2 = childIndex1 + 1;
+	if (childIndex1 >= _heap.size() || childIndex1 < 0)
+	    return -1;
+	else if (childIndex2 >= _heap.size())
+	    return childIndex1;
+	else if (_heap.get(childIndex1) < _heap.get(childIndex2))
+	    return childIndex1;
+	else
+	    return childIndex2;
+    }//O(n)
 
 
 
@@ -117,7 +157,6 @@ public class ALHeap
     //main method for testing
     public static void main( String[] args ) {
 
-	/*--V--------------MOVE ME DOWN------------------V---
 
 	  ALHeap pile = new ALHeap();
 
@@ -164,8 +203,6 @@ public class ALHeap
 	  System.out.println(pile);
 	  System.out.println("removing " + pile.removeMin() + "...");
 	  System.out.println(pile);
-
-	  ==|============================================|===*/
 
     }//end main()
 
